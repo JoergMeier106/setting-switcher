831269ef4d97c800ea66c62644b43fcae37722f6
jmeier <joerg.meier@infoteam.de>
2020-12-27 17:05:49 +0100

    First release

---
+++ README.md
@@ -0,0 +1,27 @@
+# setting-switcher README
+
+With this extension it is possible to manage different custom settings in groups. Only one group is active at a time. The active group can be selected via the status bar. This allows to change settings for different scenarios.
+
+## Features
+
+The configuration can be simply done in the settings.json file. "switchSettings.settings" is an object where each key represents a group. Each group can contain any settings. In the following example the three groups CustomSetting1, CustomSetting2 and CustomSetting3 have been defined.
+![feature defining_groups](images/settings_example.png)
+
+After the first definition you might have to reload the window.
+
+Switch the current setting group via the status bar.
+
+![feature switching_groups](images/switch_settings_example.gif)
+
+The object "switchSettings.current" will be added/changed after switching the current group.
+
+If you want to access for example the current "Setting1" use the variable:
+${config.switchSettings.current.Setting1}
+
+However, this is just an example and any group and setting names are possible.
+
+## Release Notes
+
+### 1.0.0
+
+Initial release
---
+++ src/extension.ts
@@ -0,0 +1,61 @@
+import * as vscode from 'vscode';
+
+const SETTINGS_CONFIG_NAME = 'switchSettings';
+const CURRENT_SETTING_KEY = 'extension.switchSettings.currentSetting';
+const SWITCH_SETTING_ID = 'extension.switchSettings.switchSetting';
+const CURRENT_OBJECT_KEY = 'current';
+
+let statusBarItem: vscode.StatusBarItem;
+
+function getCurrentSetting(state: vscode.Memento) : string {
+	let currentSetting = state.get<string>(CURRENT_SETTING_KEY)!;
+
+	if (currentSetting === undefined){
+		const settingKeys = Object.keys(vscode.workspace.getConfiguration(SETTINGS_CONFIG_NAME).settings);
+		if (settingKeys.length > 0){
+			currentSetting = settingKeys[0];
+			state.update(CURRENT_SETTING_KEY, currentSetting);
+		}
+		else{
+			currentSetting = "";		
+		}
+	}
+	return currentSetting;
+}
+
+async function updateCurrentSetting (currentSetting: string | undefined) {	
+	if (currentSetting !== undefined){
+		let settings = vscode.workspace.getConfiguration(SETTINGS_CONFIG_NAME);
+		let currentSettingObject = settings.settings[currentSetting];
+		await settings.update(CURRENT_OBJECT_KEY, currentSettingObject, vscode.ConfigurationTarget.Workspace);
+
+		statusBarItem.text = currentSetting;
+	}  
+}
+
+async function switchSetting(context: vscode.ExtensionContext) {
+	const settings = Object.keys(vscode.workspace.getConfiguration(SETTINGS_CONFIG_NAME).settings);
+	const currentSetting = await vscode.window.showQuickPick(settings);
+	if (currentSetting === undefined){
+		return;
+	}
+	context.workspaceState.update(CURRENT_SETTING_KEY, currentSetting);
+	updateCurrentSetting(currentSetting);
+}
+
+export function activate(context: vscode.ExtensionContext) {
+
+	let switchSettingDisposable = vscode.commands.registerCommand(SWITCH_SETTING_ID, 
+		function () { switchSetting(context); });
+
+	context.subscriptions.push(switchSettingDisposable);
+
+	const currentSetting =  getCurrentSetting(context.workspaceState);
+
+	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 800);
+	statusBarItem.command = SWITCH_SETTING_ID;
+	statusBarItem.show();
+	context.subscriptions.push(statusBarItem);
+
+	updateCurrentSetting(currentSetting);
+}
